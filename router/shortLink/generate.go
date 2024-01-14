c549500d5ff2acb15aea7cf42cd21efc2b139f02
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-01-14 15:40:09 +0800

    短链接生成算法

---
+++ router/shortLink/generate.go
@@ -0,0 +1,16 @@
+package shortLink
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+)
+
+var hasher = sha256.New()
+
+// 短链接生成算法
+func GenerateHash(uri string) string {
+	hasher.Write([]byte(uri))
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	// 截取前8个字符作为短链接标识
+	return sha[:8]
+}
---
+++ router/shortLink/generate_test.go
@@ -0,0 +1,33 @@
+package shortLink
+
+import "testing"
+
+func TestGenerateHash(t *testing.T) {
+	type args struct {
+		uri1 string
+		uri2 string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test1",
+			args: args{
+				uri1: "/",
+				uri2: "/",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g1 := GenerateHash(tt.args.uri1)
+			g2 := GenerateHash(tt.args.uri2)
+			if (g1 == g2) != tt.want {
+				t.Errorf("uri1: %s, uri2: %s, want %v", g1, g2, tt.want)
+			}
+		})
+	}
+}
