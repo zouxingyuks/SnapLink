6ff30f5fb4f736141b704b26f6ce7395752f11be
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-02-15 17:13:43 +0800

    监控功能路由

---
+++ internal/routers/linkAccessStatistic.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"SnapLink/internal/handler"
+	"github.com/gin-gonic/gin"
+)
+
+type LinkAccessStatisticHandler interface {
+	GetStatistic(c *gin.Context)
+	GetDetail(c *gin.Context)
+}
+
+func init() {
+	apiV1RouterFns = append(apiV1RouterFns, func(group *gin.RouterGroup) {
+		linkAccessStatisticRouter(group, handler.NewLinkAccessStatisticHandler())
+	})
+}
+func linkAccessStatisticRouter(group *gin.RouterGroup, h LinkAccessStatisticHandler) {
+	//获取基础访问统计(PV,UV,UIP)
+	group.GET("/linkAccessStatistic", h.GetStatistic)
+	//获取单次访问详情
+	group.GET("/linkAccessStatistic/detail", h.GetDetail)
+}
