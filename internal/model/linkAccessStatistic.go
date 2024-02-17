6934407c4acd0c5905bdc8f73a5003c243c344d1
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-02-17 17:25:51 +0800

    补充唯一索引

---
+++ internal/model/linkAccessStatistic.go
@@ -0,0 +1,23 @@
+package model
+
+import "gorm.io/gorm"
+import "gorm.io/datatypes"
+
+type LinkAccessStatistic struct {
+	gorm.Model `json:"-"`
+	URI        string         `gorm:"column:uri;type:varchar(255);commit:'访问链接';uniqueIndex:idx_query" json:"uri"`
+	Pv         int64          `gorm:"column:pv;type:bigint(20)" json:"pv"`
+	Uv         int64          `gorm:"column:uv;type:bigint(20)" json:"uv"`
+	Uip        int64          `gorm:"column:uip;type:bigint(20)" json:"uip"`
+	Datetime   string         `gorm:"column:datetime;type:datetime;uniqueIndex:idx_query" json:"datetime"`
+	Date       string         `gorm:"column:date;type:date" json:"date"`
+	Hour       int            `gorm:"column:hour;type:int(11)" json:"hour"`
+	Weekday    int            `gorm:"column:weekday;type:int(11)" json:"weekday"`
+	Regions    datatypes.JSON `gorm:"column:regions;type:json" json:"regions"`
+	IPs        datatypes.JSON `gorm:"column:ips;type:json" json:"ips"`
+	Devices    datatypes.JSON `gorm:"column:devices;type:json" json:"devices"`
+}
+
+func (l LinkAccessStatistic) TName() string {
+	return "link_access_statistics"
+}
