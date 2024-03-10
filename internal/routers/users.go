f67c45f0d1767161e095a872e8d7654414fbe97c
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-10 21:01:32 +0800

    refactor

--- internal/routers/users.go
+++ internal/routers/users.go
@@ -2,8 +2,8 @@ package routers
 
 import (
 	"SnapLink/internal/handler"
-
 	"github.com/gin-gonic/gin"
+	"github.com/zhufuyi/sponge/pkg/gin/middleware"
 )
 
 func init() {
@@ -18,29 +18,34 @@ type UsersHandler interface {
 	GetByUsernameDesensitization(c *gin.Context)
 	HasUsername(c *gin.Context)
 	Register(c *gin.Context)
-	UpdateByUsername(c *gin.Context)
+	UpdateInfo(c *gin.Context)
 	Login(c *gin.Context)
 	CheckLogin(c *gin.Context)
 	Logout(c *gin.Context)
 }
 
 func usersRouter(group *gin.RouterGroup, h UsersHandler) {
-	//group.Use(middleware.Auth()) // all of the following routes use jwt authentication
-
-	//根据用户名查找用户信息
-	group.GET("/user/:username", h.GetByUsername)
-	//根据用户名查找用户无脱敏信息
-	group.GET("/actual/user/:username", h.GetByUsernameDesensitization)
 	//查询用户名是否可用
 	group.GET("/user/has-username", h.HasUsername)
 	//注册用户
 	group.POST("/user", h.Register)
-	//修改用户
-	group.PUT("/user", h.UpdateByUsername)
 	//用户登录
 	group.POST("/user/login", h.Login)
+
 	//检查用户是否登录
 	group.GET("/user/check-login", h.CheckLogin)
+
+	group.Use(middleware.Auth()) // 需要登录的接口
+
+	//根据用户名查找用户信息
+	group.GET("/user/:username", h.GetByUsername)
+
+	//根据用户名查找用户无脱敏信息
+	group.GET("/actual/user/:username", h.GetByUsernameDesensitization)
+
+	//修改用户
+	group.PUT("/user", h.UpdateInfo)
+
 	//用户登出
 	group.DELETE("/user/logout", h.Logout)
 }
