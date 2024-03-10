7d594b41fa66c0b685a7103c715645f497f71fe5
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-10 09:08:33 +0800

    feat: bloomFilter.go 布隆过滤器基本功能

--- internal/cache/bloomFilter.go
+++ internal/cache/bloomFilter.go
@@ -1,12 +1,15 @@
 package cache
 
 import (
+	"SnapLink/internal/config"
 	"context"
 	"github.com/redis/go-redis/v9"
+	"sync"
+	"time"
 )
 
 const (
-	// cache prefix key, must end with a colon
+	// BloomFilterCachePrefixKey  布隆过滤器缓存前缀
 	BloomFilterCachePrefixKey = "BloomFilterCache:"
 )
 
@@ -22,6 +25,7 @@ type BloomFilterCache interface {
 
 	Delete(ctx context.Context, key string) error
 	Rename(ctx context.Context, key string, newKey string) error
+	Info(ctx context.Context, key string) (items, capacity int64, err error)
 }
 type bloomFilterCache struct {
 	client *redis.Client
@@ -97,6 +101,78 @@ func (b *bloomFilterCache) Rename(ctx context.Context, key string, newKey string
 	return b.client.Rename(ctx, makeBFKey(key), makeBFKey(newKey)).Err()
 }
 
+// Info 获取布隆过滤器的信息
+// items: 已插入元素数量
+// capacity: 布隆过滤器容量
+
+func (b *bloomFilterCache) Info(ctx context.Context, key string) (items, capacity int64, err error) {
+	info, err := b.client.BFInfo(ctx, makeBFKey(key)).Result()
+	if err != nil {
+		return 0, 0, err
+	}
+	return info.ItemsInserted, info.Capacity, nil
+}
+
 func makeBFKey(key string) string {
 	return BloomFilterCachePrefixKey + key
+}
+
+var defaultBloomFilterCacheInstance struct {
+	cache BloomFilterCache
+	once  sync.Once
+}
+
+func defaultBloomFilterCache() BloomFilterCache {
+	defaultBloomFilterCacheInstance.once.Do(func() {
+		opt := config.Get().BFRedis
+		defaultBloomFilterCacheInstance.cache = NewBloomFilterCache(redis.NewClient(&redis.Options{
+			Addr:         opt.Addr,
+			Password:     opt.Password,
+			DB:           opt.DB,
+			ReadTimeout:  time.Duration(opt.ReadTimeout) * time.Second,
+			WriteTimeout: time.Duration(opt.WriteTimeout) * time.Second,
+			DialTimeout:  time.Duration(opt.DialTimeout) * time.Second,
+		}))
+	})
+	return defaultBloomFilterCacheInstance.cache
+}
+
+// Create 创建布隆过滤器
+func Create(ctx context.Context, key string, errorRate float64, capacity int) error {
+	return defaultBloomFilterCache().Create(ctx, key, errorRate, capacity)
+}
+
+// Add 添加值
+func Add(ctx context.Context, key string, value string) error {
+	return defaultBloomFilterCache().Add(ctx, key, value)
+}
+
+// MAdd 批量添加多个值
+func MAdd(ctx context.Context, key string, values ...string) error {
+	return defaultBloomFilterCache().MAdd(ctx, key, values...)
+}
+
+// Exists 检查值是否存在
+func Exists(ctx context.Context, key string, value string) (bool, error) {
+	return defaultBloomFilterCache().Exists(ctx, key, value)
+}
+
+// MExists 批量检查多个值是否存在
+func MExists(ctx context.Context, key string, values ...string) ([]bool, error) {
+	return defaultBloomFilterCache().MExists(ctx, key, values...)
+}
+
+// Rename 重命名
+func Rename(ctx context.Context, key string, newKey string) error {
+	return defaultBloomFilterCache().Rename(ctx, key, newKey)
+}
+
+// Delete 删除布隆过滤器
+func Delete(ctx context.Context, key string) error {
+	return defaultBloomFilterCache().Delete(ctx, key)
+}
+
+// Info 获取布隆过滤器的信息
+func Info(ctx context.Context, key string) (items, capacity int64, err error) {
+	return defaultBloomFilterCache().Info(ctx, key)
 }
