27e6ad1699553e5dae385899b8dd870e75dc56d2
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-10 09:21:29 +0800

    fix: 补充唯一索引，用于对数据的一致性进行兜底保护

--- internal/model/tUser.go
+++ internal/model/tUser.go
@@ -7,11 +7,11 @@ import (
 
 type TUser struct {
 	ID           uint64     `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT" json:"id"`
-	Username     string     `gorm:"column:username;type:varchar(256);commit:'用户名'" json:"username"`
+	Username     string     `gorm:"column:username;type:varchar(256);commit:'用户名';uniqueIndex" json:"username"`
 	Password     string     `gorm:"column:password;type:varchar(512);commit:'密码'" json:"password"`
 	RealName     string     `gorm:"column:real_name;type:varchar(256);commit:'真实姓名'" json:"realName"`
-	Phone        string     `gorm:"column:phone;type:varchar(128);commit:'手机号'" json:"phone"`
-	Mail         string     `gorm:"column:mail;type:varchar(512);commit:'邮箱'" json:"mail"`
+	Phone        string     `gorm:"column:phone;type:varchar(128);commit:'手机号';uniqueIndex" json:"phone"`
+	Mail         string     `gorm:"column:mail;type:varchar(512);commit:'邮箱';uniqueIndex" json:"mail"`
 	DeletionTime int64      `gorm:"column:deletion_time;type:bigint(20);commit:'注销时间戳'" json:"deletionTime"` //
 	CreateTime   *time.Time `gorm:"column:create_time;type:datetime;commit:'创建时间'" json:"createTime"`        //
 	UpdateTime   *time.Time `gorm:"column:update_time;type:datetime;commit:'修改时间'" json:"updateTime"`        //
@@ -22,7 +22,7 @@ type TUser struct {
 func (u *TUser) TName() string {
 	//对 username 进行取模分表
 	id := hash(u.Username)
-	return fmt.Sprintf("t_user%d", id%16)
+	return fmt.Sprintf("t_user_%d", id%16)
 }
 
 // hash 计算字符串的哈希值
