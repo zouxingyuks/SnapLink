7a74777c5527a38700d02a6e1c97496c3cf85185
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-10 22:00:18 +0800

    refactor : 分片数参数化

--- internal/model/shardingNum.go
+++ internal/model/shardingNum.go
@@ -1,5 +1,14 @@
 package model
 
 const (
+	// TUserShardingNum 用户表分表数量
 	TUserShardingNum = 16
+	// SLGroupShardingNum 短链接分组表分表数量
+	SLGroupShardingNum = 16
+	// ShortLinkShardingNum 短链接表分表数量
+	ShortLinkShardingNum = 16
+	// LinkAccessRecordShardingNum 访问记录表分表数量
+	LinkAccessRecordShardingNum = 16
+	// LinkAccessStatisticShardingNum 访问统计表分表数量
+	LinkAccessStatisticShardingNum = 16
 )
