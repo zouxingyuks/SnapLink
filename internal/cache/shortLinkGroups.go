61819540cc587c41646967734074cd0fc16833a2
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-11 08:31:49 +0800

    feat : HSetEmpty 用于解决空值设定为问题

--- internal/cache/shortLinkGroups.go
+++ internal/cache/shortLinkGroups.go
@@ -17,6 +17,7 @@ const (
 	shortLinkGroupsCachePrefixKey = "groups:"
 	// ShortLinkGroupExpireTime expire time
 	ShortLinkGroupExpireTime = 1 * time.Hour
+	shortLinkGroupCacheEmpty = "empty"
 )
 
 var _ ShortLinkGroupCache = (*shortLinkGroupsCache)(nil)
@@ -25,6 +26,7 @@ var _ ShortLinkGroupCache = (*shortLinkGroupsCache)(nil)
 type ShortLinkGroupCache interface {
 	HSet(ctx context.Context, username string, group *model.ShortLinkGroup) error
 	HMSet(ctx context.Context, username string, groups []*model.ShortLinkGroup) error
+	HSetEmpty(ctx context.Context, username string) error
 	HGetALL(ctx context.Context, username string) ([]*model.ShortLinkGroup, error)
 	HDel(ctx context.Context, username string, gids ...string) error
 }
@@ -41,14 +43,14 @@ func NewShortLinkGroupCache(cacheType *model.CacheType) ShortLinkGroupCache {
 	}
 }
 
-// GetSLGroupKey 获取用户的 group 缓存 key
-func (c *shortLinkGroupsCache) GetSLGroupKey(username string) string {
+// makeSLGroupKey 获取用户的 group 缓存 key
+func (c *shortLinkGroupsCache) makeSLGroupKey(username string) string {
 	return shortLinkGroupsCachePrefixKey + username
 }
 
 // HSet 设置用户的 hash 缓存
 func (c *shortLinkGroupsCache) HSet(ctx context.Context, username string, group *model.ShortLinkGroup) error {
-	key := c.GetSLGroupKey(username)
+	key := c.makeSLGroupKey(username)
 	bytes, err := json.Marshal(group)
 	if err != nil {
 		return err
@@ -70,20 +72,33 @@ func (c *shortLinkGroupsCache) HMSet(ctx context.Context, username string, group
 		}
 		data[group.Gid] = string(bytes)
 	}
-	key := c.GetSLGroupKey(username)
+	key := c.makeSLGroupKey(username)
 	err := c.client.HMSet(ctx, key, data).Err()
 	return err
 }
 
+// HSetEmpty 设置用户的 hash 缓存为空
+func (c *shortLinkGroupsCache) HSetEmpty(ctx context.Context, username string) error {
+	key := c.makeSLGroupKey(username)
+	return c.client.HSet(ctx, key, shortLinkGroupCacheEmpty, "").Err()
+}
+
 // HGetALL 获取用户的 hash 缓存
 func (c *shortLinkGroupsCache) HGetALL(ctx context.Context, username string) ([]*model.ShortLinkGroup, error) {
-	key := c.GetSLGroupKey(username)
+	key := c.makeSLGroupKey(username)
 	data, err := c.client.HGetAll(ctx, key).Result()
 	if err != nil {
 		return nil, err
 	}
-	var groups []*model.ShortLinkGroup
-	for _, v := range data {
+	groups := make([]*model.ShortLinkGroup, 0, len(data))
+	for k, v := range data {
+		if k == shortLinkGroupCacheEmpty {
+			// 当且仅当只存在一个空值时，返回空
+			if len(data) == 1 {
+				return nil, nil
+			}
+			continue
+		}
 		var group model.ShortLinkGroup
 		err = json.Unmarshal([]byte(v), &group)
 		if err != nil {
@@ -96,12 +111,12 @@ func (c *shortLinkGroupsCache) HGetALL(ctx context.Context, username string) ([]
 
 // HDel 删除用户的 hash 中的某个 key
 func (c *shortLinkGroupsCache) HDel(ctx context.Context, username string, gids ...string) error {
-	key := c.GetSLGroupKey(username)
+	key := c.makeSLGroupKey(username)
 	return c.client.HDel(ctx, key, gids...).Err()
 }
 
 // Del 删除用户的 hash 缓存
 func (c *shortLinkGroupsCache) Del(ctx context.Context, username string) error {
-	key := c.GetSLGroupKey(username)
+	key := c.makeSLGroupKey(username)
 	return c.client.Del(ctx, key).Err()
 }
