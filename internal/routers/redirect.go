a039d0f844af7bfb7f9d9a0dda87b080f5b096cb
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-14 09:15:12 +0800

    feat : 短链接跳转

--- internal/routers/redirect.go
+++ internal/routers/redirect.go
@@ -2,16 +2,20 @@ package routers
 
 import (
 	"SnapLink/internal/handler"
+	"SnapLink/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
+
+type RedirectHandler interface {
+	Redirect(c *gin.Context)
+}
 
 func init() {
 	apiV1RouterFns = append(apiV1RouterFns, func(group *gin.RouterGroup) {
 		redirectRouter(group, handler.NewRedirectHandler())
 	})
 }
-func redirectRouter(group *gin.RouterGroup, h handler.RedirectHandler) {
-
-	group.Any("/:short_uri", h.Redirect)
+func redirectRouter(group *gin.RouterGroup, h RedirectHandler) {
+	group.Any("/:uri", middleware.Watcher(), h.Redirect)
 
 }
