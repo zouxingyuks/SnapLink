e742a36de9b8c9eeddf2d6b73da0507999271f1c
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2024-03-16 23:47:11 +0800

    feat : 短链接监控中间件

--- internal/middleware/LinkStatusWatcher.go
+++ internal/middleware/LinkStatusWatcher.go
@@ -3,12 +3,14 @@ package middleware
 import (
 	"SnapLink/internal/cache"
 	"SnapLink/internal/dao"
+	"SnapLink/internal/messageQueue"
 	"SnapLink/internal/model"
 	"context"
-	"fmt"
+	"github.com/ThreeDotsLabs/watermill-amqp/pkg/amqp"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/pkg/errors"
-	"net/http"
+	"github.com/zhufuyi/sponge/pkg/logger"
 	"sync"
 	"time"
 )
@@ -25,8 +27,13 @@ var weekDay = map[string]int{
 
 func Watcher() gin.HandlerFunc {
 	watcherInstance.Once.Do(func() {
-		watcherInstance.iDao = dao.NewLinkStatsDao(
+		var err error
+		watcherInstance.sDao = dao.NewLinkAccessStatisticDao(
 			cache.NewLinkStatsCache(model.GetCacheType()))
+		watcherInstance.publisher, err = messageQueue.NewPublisher()
+		if err != nil {
+			logger.Panic(errors.Wrap(err, "init rocketmq...failed").Error())
+		}
 	})
 	return watcherInstance.Do()
 }
@@ -35,16 +42,18 @@ var watcherInstance watcher
 
 // LinkStatsDao defining the dao interface
 type LinkStatsDao interface {
-	Get(ctx context.Context, originalUrl string, date string, hour int) (*model.LinkAccessStat, error)
-	Set(ctx context.Context, data *model.LinkAccessStat) error
-	UpdateUip(ctx context.Context, originalUrl string, date string, hour int, ip string) error
-	UpdateUv(ctx context.Context, originalUrl string, date string, hour int) error
-	UpdatePv(ctx context.Context, originalUrl string, date string, hour int) error
+	UpdateIp(ctx context.Context, uri string, date string, hour int, ip string) error
+	UpdateUv(ctx context.Context, uri string, date string, hour int) error
+	UpdateUA(ctx context.Context, uri string, date string, hour int, browser, device string) error
+	UpdatePv(ctx context.Context, uri string, date string, hour int) error
+	SaveAccessRecord(ctx context.Context, record *model.LinkAccessRecord) error
+	UpdateLocation(ctx context.Context, uri string, date string, hour int, location string) error
 }
 
 type watcher struct {
-	iDao LinkStatsDao
+	sDao LinkStatsDao
 	sync.Once
+	publisher *amqp.Publisher
 }
 
 func (w *watcher) Do() gin.HandlerFunc {
@@ -52,74 +61,20 @@ func (w *watcher) Do() gin.HandlerFunc {
 		c.Next()
 		// 此处是用于监控短链接的访问情况，其成功访问的状态码为302
 		if c.Writer.Status() == 302 {
-			var (
-				err    error
-				status *model.LinkAccessStat
-			)
-			originalURL := c.Writer.Header().Get("Location")
+			//todo 发布到消息队列：AccessLog
+			info := c.MustGet("info").(*model.Redirect)
+			header := c.Request.Header
+			ip := c.RemoteIP()
+			uid, err := c.Cookie("uid")
+			if err != nil {
+				uid = uuid.NewString()
 
-			// 获取目前的短链接统计情况
-			{
-				// 通过短链接的uri获取短链接的信息，以小时为单位统计
-				status, err = w.iDao.Get(c,
-					originalURL,
-					time.Now().Format("2006-01-02"),
-					weekDay[time.Now().Weekday().String()]*24+(time.Now().Hour()))
-				status.Date = time.Now().Format("2006-01-02")
-				status.Hour = time.Now().Hour()
-				if err != nil {
-					// todo 记录日志
-				}
+				c.SetCookie("uid", uid, 3600, "/", "", false, false)
 			}
-			// 更新短链接的统计情况
-			// pv监控
-			{
-				w.iDao.UpdatePv(c, originalURL, status.Date, status.Hour)
+			err = w.publisher.Publish("AccessLog", messageQueue.NewAccessLogMessage(*info, header, ip, uid, time.Now().String()))
+			if err != nil {
+				logger.Err(err)
 			}
-			// uv监控
-			{
-				//根据 uv 的存在性与否来判断是否需要更新 uv
-				_, err := c.Cookie("uv")
-				if errors.Is(err, http.ErrNoCookie) {
-					w.iDao.UpdateUv(c, originalURL, status.Date, status.Hour)
-				}
-			}
-			// ip监控&&地理位置监控
-			{
-				//获取真实 ip
-				//todo 优化此获取方法，目前基于gin自带的获取ip的方法来去获取真实的ip
-				ip := c.ClientIP()
-				err := w.iDao.UpdateUip(c, originalURL, status.Date, status.Hour, ip)
-				if err != nil {
-					//如果更新出错，则代表redis出错
-				}
-				//通过ip获取地理位置
-				//locationInfos, err := ipSearcher.Do(ip)
-				//exist, err := w.iDao.ExistOrAddLocation(c, originalURL, status.Date, status.Hour, locationInfos["city"])
-				//if err != nil {
-				//	// todo 记录日志
-				//	fmt.Println("获取ip失败", err)
-				//}
-				//if !exist {
-				//	//todo 如何对某个区域进行统计
-				//	status.Uip++
-				//}
-			}
-			// UA 监控
-			{
-				//ua := c.GetHeader("User-Agent")
-
-				//if err != nil {
-				//	// todo 记录日志
-				//	fmt.Println("获取ua失败", err)
-				//}
-				//if !exist {
-				//	//对 ua 进行统计
-				//}
-			}
-			// 更新短链接的统计情况
-			//err = watcherInstance.iDao.Set(c, status)
-			fmt.Printf("%+v", status)
 		}
 	}
 }
